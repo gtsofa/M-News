0d2cf3dbcd023dbe967239012994cec4a30dd49a
gtsofa <jlsnyule@yahoo.com>
2021-04-06 18:59:06 +0300

    Initial commit

---
+++ M-NewsTests/M_NewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  M_NewsTests.swift
+//  M-NewsTests
+//
+//  Created by Julius on 06/04/2021.
+//
+
+import XCTest
+@testable import M_News
+
+class M_NewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
